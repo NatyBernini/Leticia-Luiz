738452a02bc65f60bc39caac83db13c140509c16
NatyBernini <110041227+NatyBernini@users.noreply.github.com>
2026-05-19 19:04:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,257 @@
+// Array de objetos contendo os dados de cada presente
+const presentes = [
+    {
+        nome: "Travessa e Assadeira Refratárias com Alça 3un Porcelana Branca",
+        imagem: "img/2.png",
+        link: "hhttps://s.shopee.com.br/5L8Ub6PCEF"
+    },
+      {
+        nome: "Faqueiro 20, 25, 30, 35, 48 ou 54 peças de aço inox prateado com Gift Box Viena Wolff",
+        imagem: "img/3.png",
+        link: "https://s.shopee.com.br/7pppZpl1gu"
+    },
+      {
+        nome: "Jogo De Facas Inox Tramontina 4 7 10 Peças 5 Peças Com Cepo de Madeira Plenus Preto",
+        imagem: "img/4.png",
+        link: "https://s.shopee.com.br/16yFSoBuB"
+    },
+      {
+        nome: "Conjunto Bowls de Inox com Tampa Electrolux",
+        imagem: "img/5.png",
+        link: "https://s.shopee.com.br/4qCE0MIBhl"
+    },
+       {
+        nome: "Chaleira Elétrica Electrolux 1,8L Efficient com Desligamento Automático (EEK10)",
+        imagem: "img/6.png",
+        link: "https://s.shopee.com.br/gMf2iTFL7"
+    },         
+    {
+        nome: "5 Pano Prato Cozinha Xadrez Branco c/ Dourado Grande Algodão Macio Laune Haus",
+        imagem: "img/8.png",
+        link: "https://s.shopee.com.br/2g7jQPypHr"
+    },
+        {
+        nome: "Jogo De Taças De Vidro Diamond Cristal 325ml 6 Peças",
+        imagem: "img/9.png",
+        link: "https://s.shopee.com.br/8ASfyXQHjG"
+    },
+            {
+        nome: "Jarra Diamond De Vidro Com Fio De Ouro Dourado 1,14L Lyor",
+        imagem: "img/10.png",
+        link: "https://s.shopee.com.br/8fOwZTSNRA"
+    },
+                {
+        nome: "Boleira Porta Bolo Bambu Com Tampa Cúpula Em Acrílico 28cm",
+        imagem: "img/11.png",
+        link: "hhttps://s.shopee.com.br/6L11nCJKGF"
+    },
+                    {
+        nome: "Porta Queijo Queijeira Madeira Bambu Com Tampa 19cm X 9cm Em Acrílico Mesa Posta",
+        imagem: "img/12.png",
+        link: "https://s.shopee.com.br/1LcLq1xxHm"
+    },                    {
+        nome: "Garrafa Térmica Termometro Digital 1L Café Chá Nórdica Com Cabo Madeira",
+        imagem: "img/13.png",
+        link: "https://s.shopee.com.br/3LNQDixV1a"
+    },                   {
+        nome: "Jogo de Taças de Cerveja Floripa com 6 peças 300ml",
+        imagem: "img/14.png",
+        link: "https://s.shopee.com.br/6L11nFrF7u"
+    },
+                    {
+        nome: "Jogo 6 Taças de Vidro Premium Resistente para Vinho e Água 465ml - Design Moderno Drink Coquetel",
+        imagem: "img/15.png",
+        link: "hhttps://s.shopee.com.br/gMf2rtrJc"
+    },
+         {
+        nome: "kit 02 Travesseiros Altenburg Antistress Tech 0,50x0,70m",
+        imagem: "img/16.png",
+        link: "https://s.shopee.com.br/20s2dLAvZO"
+    },
+             {
+        nome: "Bandeja de Bambu e Madeira com Alças Douradas -Um Design Sofisticado Adequado para Todas as Ocasiões",
+        imagem: "img/17.png",
+        link: "https://s.shopee.com.br/1qYcR3kQOV"
+    },
+                 {
+        nome: "Jogo de Utensílios Tramontina Softta em Silicone 7 Peças",
+        imagem: "img/18.png",
+        link: "https://s.shopee.com.br/8ASfyiTTKX"
+    },
+                     {
+        nome: "Cj 6 Xícaras Café em Porcelana Com Pires Paddy Branco 90Ml - Wolff Original",
+        imagem: "img/19.png",
+        link: "https://s.shopee.com.br/1qYcR5vZvR"
+    },
+                         {
+        nome: "Jogo 12 Potes Hermetico Vidro Mantimentos Tampa Bambu",
+        imagem: "img/20.png",
+        link: "https://s.shopee.com.br/8V5WNMOAG9"
+    },
+                             {
+        nome: "Kit de 6 até 12 Potes de Vidro 640ml - 370 ml Hermético Marmita Fit com Tampa 4 Travas Premium",
+        imagem: "img/21.png",
+        link: "https://s.shopee.com.br/2qR9cxnBNm"
+    },
+                                 {
+        nome: "Suporte de Panela Dobrável em Madeira Formato de Árvore | Suporte para Panelas e Pratos Quentes | Trivet de Bambu",
+        imagem: "img/22.png",
+        link: "https://s.shopee.com.br/5flL0Bkn9q"
+    },
+                                     {
+        nome: "Porta Alho Feito de Cerâmica Premium Nome em Alto Relevo Tendencia Varias Cores",
+        imagem: "img/23.png",
+        link: "https://s.shopee.com.br/3g0GcY9ocb"
+    },
+                                         {
+        nome: "Grill Sanduicheira Cadence SAN400 Elétrica Chapa Misteira Tostex Tostequeira Hambúrguer Click 127v",
+        imagem: "img/24.png",
+        link: "https://s.shopee.com.br/8V5WNRmXjA"
+    },
+    {
+        nome: "Kit Petisqueira Em Bambu + Facas E Garfos Tábua Ecoeden",
+        imagem: "img/27.png",
+        link: "https://s.shopee.com.br/5flL0JquT1"
+    },
+                                                        {
+        nome: "Kit 2 Dispenser Hermético 3L com Dosador para Cozinha Ou Lavanderia",
+        imagem: "img/28.png",
+        link: "https://s.shopee.com.br/8fOwZqVjfc"
+    },
+                                                            {
+        nome: "Manteigueira de Porcelana Branca com Tampa de Bambu e Espátula Elegante KL-PJ03629-S",
+        imagem: "img/29.png",
+        link: "https://s.shopee.com.br/4AwXDaqFAy"
+    },
+    {
+        nome: "Kit 4 Super Panos De Chão Grandes Em Microfibra Panão 80x50",
+        imagem: "img/39.png",
+        link: "https://s.shopee.com.br/5flL0NgYyq"
+    },
+    {
+        nome: "Porta Ovos Acrílico Organizador Geladeira Gaveteiro Dupla Capacidade Cor Branca Envio Imediato",
+        imagem: "img/30.png",
+        link: "https://shopee.com.br/product/1333478822/22393835046?d_id=acce1&uls_trackid=55ilj36602k0&utm_content=27xTz6wknhgP85hLspNNENFxagLX"
+    },
+    {
+        nome: "Belifou Kit 6 Pote hermético 200ML Pote De Vidro com Tampa bambu pote de mantimentos para cozinha",
+        imagem: "img/31.png",
+        link: "https://shopee.com.br/product/1224363395/22098063316?d_id=acce1&uls_trackid=55ilj8so01kl&utm_content=27xTz6wknhivu4vEQF5hHM8LcrK1"
+    }, 
+    {
+        nome: "Jogo 6 Taças Vinho Tinto Em Cristal Bohemia 650 Ml Titanium Cor Transparente",
+        imagem: "img/34.png",
+        link: "https://shopee.com.br/product/1224363395/22098063316?d_id=acce1&uls_trackid=55ilj8so01kl&utm_content=27xTz6wknhivu4vEQF5hHM8LcrK1"
+    },
+                            {
+        nome: "Jogo 12 / 06 Taça Vidro Cristalino Buffet 186ML Champanhe Espumante Vinho Branco Bar Restaurante",
+        imagem: "img/35.png",
+        link: "https://shopee.com.br/product/1224363395/22098063316?d_id=acce1&uls_trackid=55ilj8so01kl&utm_content=27xTz6wknhivu4vEQF5hHM8LcrK1"
+    },
+                                {
+        nome: "Jogo De 12 Copos 320ml Vidro Bali Quadrado Luxo Transparente",
+        imagem: "img/36.png",
+        link: "https://shopee.com.br/product/1224363395/22098063316?d_id=acce1&uls_trackid=55ilj8so01kl&utm_content=27xTz6wknhivu4vEQF5hHM8LcrK1"
+    },
+    {
+        nome: "Saladeira Bowl Ryo Maresia 1,6 Litros 26 cm Oxford Porcelana",
+        imagem: "img/40.png",
+        link: "https://shopee.com.br/product/389126594/22897548128?d_id=acce1&uls_trackid=55ilqe2n00l4&utm_content=27xTz6wknkWgat1P9w9nkmJHbCmD"
+    },
+                                                {
+        nome: "Conjunto de Utensílios de Bambu de 06 Peças",
+        imagem: "img/41.png",
+        link: "https://shopee.com.br/product/1526836548/23094041212?d_id=acce1&uls_trackid=55ilqiau01kl&utm_content=27xTz6wknkkm1kxw8JRZj1WczBdy"
+    },
+                                                    {
+        nome: "Porta Pães Com Tampa Retrátil de Bambu Ecokitchen Mimo Style",
+        imagem: "img/42.png",
+        link: "https://shopee.com.br/product/801047504/19117762707?d_id=acce1&uls_trackid=55ilqlfm020t&utm_content=27xTz6wknkpFuvtCxyLuUw7khWAs"
+    },
+                                                        {
+        nome: "Kit Jogo Americano Sousplat Bordado Suplat de Mesa Posta Redondo/Retangular e Guardanapos",
+        imagem: "img/43.png",
+        link: "https://shopee.com.br/product/1314421697/22194388384?d_id=acce1&uls_trackid=55ilqpnf00k6&utm_content=27xTz6wknkzkcSe2Uz6vk9nDLu6w"
+    },
+                                                            {
+        nome: "Porta Frios Bambu Duplo Com Divisorias Tampa de Acrilico",
+        imagem: "img/44.png",
+        link: "https://shopee.com.br/product/1504918650/21398149079?d_id=acce1&uls_trackid=55ilqube01oi&utm_content=27xTz6wknmQScvxPhkL3vuTQ7A3h"
+    },
+{
+        nome: "Varal",
+        imagem: "img/48.png",
+        link: "https://shopee.com.br/product/1504788346/18398346899?exp_group=rollout&gads_t_sig=gqRjZGVrxHCFomtpsTE0MjUxOnRzc19zZGtfa2V5omt20QABpGFsZ2_SAAAAZKNkZWvAomN0xEAAAAAMRoz0ZUjQw0QlRa--FjB0AKnHQPF7xv4DyGj9-GQwqn4zSdB6gztmw7ebmtsZs9FPJxlVqctc57WUE3IRqmNpcGhlcnRleHTEmQAAAAxGP8AjsJZGzuSpiETMZPLkBQaZXNfqclzhlh4mGlG41Jj3BX6hEK05xy2_03XZEeRV1bFJxO58QuGx0KaNHcnrdW3kbChuQE3eht1AXus9l2ps_WqopTf352O1MEvWuSEFe9vm4QvHRHmtpw4YNOC915g-7wahZ4SkbNFBfIw7kHu7XwOrUVtn0YyznR_i63DD4f3EsA&mmp_pid=an_18389281073&share_channel_code=1&uls_trackid=55kl4n4a00l0&utm_campaign=id_7Yv9iiwaGl&utm_content=----&utm_medium=affiliates&utm_source=an_18389281073&utm_term=evzm1o833d2w"
+    },
+
+];
+
+const container = document.getElementById('lista-presentes');
+
+function renderizarPresentes() {
+    presentes.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'card-presente';
+
+        card.innerHTML = `
+            <div class="img-container">
+                <img src="${item.imagem}" alt="${item.nome}">
+            </div>
+            <div class="card-info">
+                <h3>${item.nome}</h3>
+                <a href="${item.link}" target="_blank" class="btn-presentear">Presentear</a>
+            </div>
+        `;
+
+        container.appendChild(card);
+    });
+}
+
+const musica = document.getElementById('musica-tema');
+const btnAudio = document.getElementById('btn-audio');
+const audioIcon = document.getElementById('audio-icon');
+const volumeSlider = document.getElementById('volume-control');
+
+// Função que você já tem, atualizada para iniciar o volume do slider
+function fecharModal() {
+    document.getElementById('modal-aviso').style.display = 'none';
+    
+    if (musica) {
+        musica.volume = volumeSlider.value;
+        musica.play().catch(e => console.log("Autoplay bloqueado pelo browser"));
+    }
+}
+
+// Alternar entre Mudo e Som
+function toggleMute() {
+    if (musica.muted) {
+        musica.muted = false;
+        audioIcon.innerText = "🔊";
+    } else {
+        musica.muted = true;
+        audioIcon.innerText = "🔇";
+    }
+}
+
+// Controle de Volume em tempo real
+volumeSlider.addEventListener('input', (e) => {
+    const vol = e.target.value;
+    musica.volume = vol;
+    
+    if (vol == 0) {
+        audioIcon.innerText = "🔇";
+    } else {
+        audioIcon.innerText = "🔊";
+    }
+});
+
+function copiarPix() {
+    const chave = document.getElementById('pix-chave').innerText;
+    navigator.clipboard.writeText(chave).then(() => {
+        const msg = document.getElementById('msg-copiado');
+        msg.style.display = 'block';
+        setTimeout(() => { msg.style.display = 'none'; }, 2000);
+    });
+}
+// Inicia a renderização ao carregar a página
+document.addEventListener('DOMContentLoaded', renderizarPresentes);
